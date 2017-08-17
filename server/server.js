0eddabe20231c5749642fd6f2505b8e50b884dab
tuanpham <pvtuanhcmus@gmail.com>
2017-08-17 16:42:51 +0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -5,29 +5,50 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
-
 var server = http.createServer(app);
 var io = socketIO(server);
 
+
 app.use(express.static(publicPath));
+
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.on('createMessage',(mesage) => {
-		io.emit('newMessage',{
-			from: mesage.from,
-			text: mesage.text,
-			createAt: new Date().getTime()
-		})
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createAt: new Date().getTime()
+	});
+
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createAt: new Date().getTime()
 	})
+
+
+	socket.on('createMessage',(mesage) => {
+		console.log('createMessage',mesage);
+		// io.emit('newMessage',{
+		// 	from: mesage.from,
+		// 	text: mesage.text,
+		// 	createAt: new Date().getTime()
+		// })
+
+		socket.broadcast.emit('newMessage',{
+			from: message.from,
+			text: message.text,
+			createAt: new Date().getTime()
+		});
+	});
 
 
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
-	})
+	});
 })
 
 server.listen(port,() => {
